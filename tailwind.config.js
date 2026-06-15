9ec90e0786a7658ea301ed35c10387b769a05379
ecomsys <ecomsysru@gmail.com>
2026-06-15 08:58:30 +0300

    update 15.06

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ export default {
         white: "#fff",
         golden: "#ffc356",
         goluboy: "#00b5d1",
+        seriy:"#F7F3EB",
         // Grayscale (используется для gray-* классов)
         grayscale: {
           25: "#f8f9f9",
